08ab18650430e88bb1bf071a91924439ba00becb
Mickey <79796196+Mickey758@users.noreply.github.com>
2021-08-29 02:15:54 -0400

    update

--- static/index/main.js
+++ static/index/main.js
@@ -1,11 +1,3 @@
 function test_username(){
-    var request = new XMLHttpRequest();
-    request.open("GET","https://api.agalts.xyz/v1/username?format=text",true);
-    request.send();
-    request.onload = function(){
-        Swal.fire({
-            title:"GET api.agalts.xyz/v1/username?format=text",
-            text:"RESPONSE "+request.response
-        })
-    }
+    window.location = "https://api.agalts.xyz/v1/username?format=text"
 }
